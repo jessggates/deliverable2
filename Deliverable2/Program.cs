6be036ece78e56e58035bb8b7143d72ac4d452b1
Jessica Gates <gatespj@outlook.com>
2023-12-27 13:49:13 -0500

    committing heads or tails game

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -1,34 +1,43 @@
 ﻿{
-    Console.WriteLine("Welcome to the COIN FLIP CHALLENGE!");
-    Console.WriteLine("What is your name? ");
-    string name = Console.ReadLine();
-    Console.WriteLine("Welcome " + name + "! " + "Do you want to do the COIN FLIP CHALLENGE? yes/no");
-    string answer = Console.ReadLine();
-
-    if (answer == "yes")
     {
-        for (int x = 5; x >= 1; x--)
+        int score = 0;
+        Console.WriteLine("Welcome to the COIN FLIP CHALLENGE!");
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Welcome " + name + "! " + "Do you want to do the COIN FLIP CHALLENGE? yes/no");
+        string answer = Console.ReadLine();
+
+        if (answer == "yes")
         {
-            Random random = new Random();
-            int test = random.Next(0, 2);
-            Console.WriteLine("Heads or Tails?");
-            string coinChoice = Console.ReadLine();
-            if (test == 0 && coinChoice.ToLower() == "heads")
+            for (int x = 1; x <= 5; x++)
             {
-                Console.WriteLine("Correct!");
+                Random random = new Random();
+                int test = random.Next(0, 2);
+                Console.WriteLine("Heads or Tails?");
+                string coinChoice = Console.ReadLine();
+                if (test == 0 && coinChoice == "Heads")
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else if (test == 1 && coinChoice == "Tails")
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong!");
+                }
             }
-            else if (test == 1 && coinChoice.ToLower() == "tails")
             {
-                Console.WriteLine("Correct!");
-            }
-            else
-            {
-                Console.WriteLine("Wrong!");
+                Console.WriteLine("Thank you " + name + "! You got a score of " + score);
             }
         }
-    }
-    else
-    {
-        Console.WriteLine("You are a coward " + name + "! ");
+        else
+        {
+            Console.WriteLine("You are a coward " + name + "! ");
+        }
     }
 }
